069fde7cbe5ac7d0a2d842ca2328069a20915453
MarcinD12 <dickdaring0@gmail.com>
2023-06-17 10:36:07 +0200

    grande finale

--- OnionCore/Models/Part.cs
+++ OnionCore/Models/Part.cs
@@ -1,14 +1,15 @@
-﻿namespace OnionCore.Models
+﻿using System.Text.Json.Serialization;
+namespace OnionCore.Models
 {
     public class Part
     {
         public int PartId { get; set; }
         public string PartName { get; set; }
         public int Price { get; set; }
-        [Newtonsoft.Json.JsonIgnore]
+        [JsonIgnore]
         public Supplier? Supplier { get; set; }
         public int? SupplierId { get; set; }
-        [Newtonsoft.Json.JsonIgnore]
+        [JsonIgnore]
         public ICollection<Stock> Stocks { get; set; }
 
     }
--- OnionCore/Models/Warehouse.cs
+++ OnionCore/Models/Warehouse.cs
@@ -1,9 +1,11 @@
-﻿namespace OnionCore.Models
+﻿using System.Text.Json.Serialization;
+namespace OnionCore.Models
 {
     public class Warehouse
     {
         public int WarehouseId { get; set; }
         public string WarehouseName { get; set; }
+        [JsonIgnore]
         public ICollection<Stock> Stocks { get; set; }
 
 
